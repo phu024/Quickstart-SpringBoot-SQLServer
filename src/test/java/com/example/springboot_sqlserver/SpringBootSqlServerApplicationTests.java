5ec71f85603cd68e87a850e5de5efccfc3f45f75
Phuwadon Decharam <phuwadon.dec@gmail.com>
2022-11-21 15:36:55 +0700

    Quickstart SpringBoot connect SQLServer

---
+++ src/test/java/com/example/springboot_sqlserver/SpringBootSqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_sqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSqlServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
